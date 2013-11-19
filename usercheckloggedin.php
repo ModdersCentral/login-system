c0f6afd5d34483cb6a6994a0cca645949490b2d3
jamie scott <webmaster@modderscentral.com>
2013-11-19 20:59:46 +0000

    spelling mistakes plus. optimised

--- usercheckloggedin.php
+++ usercheckloggedin.php
@@ -1,6 +1,6 @@
 <?php include('./settings/main.php');
 if (medium_checkloggedin()){
-echo "not an logged.";//fail - with the error
+echo "not logged in.";//fail - with the error
 }else{
 echo "i think im logged in as admin."; //success
 }
