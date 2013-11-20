e7f0e7223c23b408289c6f1bb6283fccbf208597
jamie scott <webmaster@modderscentral.com>
2013-11-20 00:06:04 +0000

    added: valid customer reference id

---
+++ store.php
@@ -0,0 +1,8 @@
+<?php include('./settings/main.php');
+///TODO:
+///if id is valid show the upgrade form
+///we will use the id as a reference code and auto-fill  it in a text-box for easy access
+
+
+echo htmlentities($_GET['id']);//this code is temporary just to show you
+?>
